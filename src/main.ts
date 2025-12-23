d2cc557747288cb56b73209bbbf0ae9431354f69
tDerek4u <zwehtetmhan433@gmail.com>
2025-12-23 22:57:03 +0700

    nestjs setup

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap:', err);
+});
