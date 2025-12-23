a87991bbc1bf5cc41d3e6042322438cc7c895517
tDerek4u <zwehtetmhan433@gmail.com>
2025-12-24 00:22:07 +0700

    nestjs controller users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: { name: string; age: number }) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() user: { name?: string; age?: number },
+  ) {
+    return { id, ...user };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
