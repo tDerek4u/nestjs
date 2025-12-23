95106439c5ff1139ea140d0a5dd370e205ebab49
tDerek4u <zwehtetmhan433@gmail.com>
2025-12-24 01:01:28 +0700

    nest provider

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,35 +6,38 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
-
+import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
-  findAll() {
-    return [];
+  findAll(@Query('role') role?: 'admin' | 'user') {
+    return this.usersService.findAll(role);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(Number(id));
   }
 
   @Post()
-  create(@Body() user: { name: string; age: number }) {
-    return user;
+  create(@Body() user: { name: string; age: number; role: 'admin' | 'user' }) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id')
   update(
     @Param('id') id: string,
-    @Body() user: { name?: string; age?: number },
+    @Body() user: { name?: string; age?: number; role?: 'admin' | 'user' },
   ) {
-    return { id, ...user };
+    return this.usersService.update(Number(id), user);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(Number(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'John Doe', age: 30, role: 'admin' },
+    { id: 2, name: 'Jane Smith', age: 25, role: 'user' },
+  ];
+
+  findAll(role?: 'admin' | 'user') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(user: { name: string; age: number; role: 'admin' | 'user' }) {
+    const maxId = this.users.reduce((max, u) => (u.id > max ? u.id : max), 0);
+    const newUser = {
+      id: maxId + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: { name?: string; age?: number; role?: 'admin' | 'user' },
+  ) {
+    this.users = this.users.map((user) =>
+      user.id === id ? { ...user, ...updatedUser } : user,
+    );
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
+  }
+}
